6e8ad8ee78dea9152db342123d033e315d4ecab3
Jason Millward <jason@jcode.me>
2012-10-26 18:00:43 +1030

    Modified base.py

--- resMon/base.py
+++ resMon/base.py
@@ -52,9 +52,9 @@ def shiftDec(x):
     for y in range(1, 16):
         if x > 1:
             x = x - 1
-            bits = "0" + bits
+            bits = "1" + bits
         else:
-            bits = "1" + bits 
+            bits = "0" + bits 
 
     print bits
     
