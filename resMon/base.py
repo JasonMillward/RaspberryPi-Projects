5db8460d332a590c437ff0880b6b368f91ae7d22
Jason Millward <jason@jcode.me>
2012-10-26 15:25:45 +1030

    Modified base.py

--- resMon/base.py
+++ resMon/base.py
@@ -56,7 +56,7 @@ def main():
 
     print psutil.phymem_usage()
     
-    print psutil.cpu_times()
+    print psutil.cpu_percent(interval=1, percpu=True)
     #init()    
     #for frame in cycle(read_lines(filename)):
     #        #print frame
