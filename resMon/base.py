24feee73d652cdb6bc4c1060e8b75ce7e35bf6b3
Jason Millward <jason@jcode.me>
2012-10-26 16:12:44 +1030

    Modified base.py

--- resMon/base.py
+++ resMon/base.py
@@ -51,6 +51,7 @@ def read_lines(filename):
     with open(filename) as f:
         for line in f:
             yield line.strip()
+            
 def dec_to_bit(x):
     output = ""
     for y in range(1, 16):
