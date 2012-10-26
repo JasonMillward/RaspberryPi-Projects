f6ca8a45ebc422f567c208eabd4d8fc50e2c0771
Jason Millward <jason@jcode.me>
2012-10-26 14:44:55 +1030

    Modified base.py

---
+++ resMon/base.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+# Take cpu load percentate, *100 / 6.25
+
+
+from itertools import cycle
+from time import sleep
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+
+
+# Connect the shift register pins to these GPIO pins.
+data_pin = 17
+clock_pin = 21
+latch_pin = 22
+
+
+GPIO.setup(data_pin, GPIO.OUT)
+GPIO.setup(data_pin, GPIO.OUT)
+GPIO.setup(data_pin, GPIO.OUT)
+
+def digital_write(pin, value):
+    GPIO.output(latch_pin, True)
+    path = '/sys/class/gpio/gpio%d/value' % pin
+        with open(path, 'w') as f:
+                f.write('1' if value else '0')
+
+def init():
+        digital_write(data_pin, 0)
+        digital_write(clock_pin, 0)
+        digital_write(latch_pin, 0)
+
+def shift_bit(value):
+        digital_write(data_pin, value)
+        digital_write(clock_pin, 1)
+        digital_write(clock_pin, 0)
+
+def latch():
+        digital_write(latch_pin, 1)
+        digital_write(latch_pin, 0)
+
+def read_lines(filename):
+        with open(filename) as f:
+                for line in f:
+                        yield line.strip()
+
+def main():
+        init()
+        filename = 'animation.txt'
+        for frame in cycle(read_lines(filename)):
+                #print frame
+                for pixel in frame:
+                        shift_bit(pixel == '#')
+                latch()
+                sleep(0.05)
+
+if __name__ == '__main__':
+        main()
